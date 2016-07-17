582c3a88fe3310f3daf487f7c1e80bb81879c933
David Llop <llop-no-spam-please@protonmail.com>
2016-07-17 21:32:04 +0200

    Fork to ipfs-vanity

--- README.md
+++ README.md
@@ -1,16 +1,35 @@
-# ipfs-key
-A tool for easily generating ipfs keypairs. When run, it will write the bytes of
-the serialized private key to stdout. By default, a 2048 bit RSA key will be
-generated. The keysize can be changed by specifying the `-size` option, and the
-key type can be changed by specifying the `-type` option (currently only RSA is
-implemented).
+# ipfs-vanity
+
+**WARNING:** This is experimental software and it is not optimized. Use it at
+your own risk.
+
+ipfs-vanity lets you create vanity keyspairs for IPFS. You can customize the
+first letters of your IPNS address.
+
+When run, it will write the bytes of the serialized private key to stdout.
+
+Use the `-name` option to specify the letters you want after the `Qm` part.
+
+By default, a 2048 bit RSA key will be generated. The keysize can be changed by
+specifying the `-bitsize` option, and the key type can be changed by specifying
+the `-type` option (currently only RSA is implemented).
 
 ## Installation
 ```
-$ go get github.com/whyrusleeping/ipfs-key
+$ go get github.com/llopv/ipfs-vanity
 ```
 
 ## Usage
 ```
-$ ipfs-key -size=4096 > my.key
+$ ipfs-vanity -name IPFS > my.key
+Generating some 4096 bit RSA keys...
+.................................................. 500
+.................................................. 1000
+.................................................. 1500
+.................................................. 2000
+.................................................. 2500
+.................................................. 3000
+...
+Success!
+ID for generated key: QmIPFSbevfCChmpuhbNMtnnghiQozMfd4ZL2grNu996p4s
 ```
--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"regexp"
 
 	ci "github.com/ipfs/go-libp2p-crypto"
 	peer "github.com/ipfs/go-libp2p-peer"
 )
 
 func main() {
+	vanity := flag.String("name", "", "write the name to appear in the begining of the key")
 	size := flag.Int("bitsize", 2048, "select the bitsize of the key to generate")
 	typ := flag.String("type", "RSA", "select type of key to generate")
 
@@ -24,21 +26,49 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Fprintf(os.Stderr, "Generating a %d bit %s key...\n", *size, *typ)
-	priv, pub, err := ci.GenerateKeyPair(atyp, *size)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
-	fmt.Fprintln(os.Stderr, "Success!")
-
-	pid, err := peer.IDFromPublicKey(pub)
+	r, err := regexp.Compile("^Qm" + *vanity + ".+$")
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 
-	fmt.Fprintf(os.Stderr, "ID for generated key: %s\n", pid.Pretty())
+	fmt.Fprintf(os.Stderr, "Generating some %d bit %s keys...\n", *size, *typ)
+
+	var priv ci.PrivKey
+	var pub ci.PubKey
+	var id = ""
+	var i = 0
+
+	for !r.MatchString(id) {
+
+		priv, pub, err = ci.GenerateKeyPair(atyp, *size)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		pid, err := peer.IDFromPublicKey(pub)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		id = pid.Pretty()
+
+		i++
+
+		if i % 10 == 0 {
+			fmt.Fprintf(os.Stderr, "%s", ".")
+		}
+
+		if i % 500 == 0 {
+			fmt.Fprintln(os.Stderr, "", i)
+		}
+	}
+
+	fmt.Fprintln(os.Stderr, "\nSuccess!")
+
+	fmt.Fprintf(os.Stderr, "ID for generated key: %s\n", id)
 
 	data, err := priv.Bytes()
 	if err != nil {
